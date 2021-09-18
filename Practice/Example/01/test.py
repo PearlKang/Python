62e28b35261457d0b04a11f405e021f20aec0156
PearlKang <rksk1588@gmail.com>
2021-09-18 21:18:02 +0900

    210918
    update 2

---
+++ Practice/Example/01/test.py
@@ -0,0 +1,44 @@
+if 4 in [1, 2, 3, 4]:
+    print("4가 있습니다.")
+
+print("")
+
+print(1 + 1)
+print(1 + 2)
+print(3 / 2.4)
+print(3 * 9)
+
+print("")
+
+a = 1
+b = 2
+print(a + b)
+
+print("")
+
+a = "Python"
+print(a)
+
+print("")
+
+a = 3
+if a > 1:
+    print("a is greater than 1")
+
+print("")
+
+for a in [1 ,2 ,3]:
+    print(a)
+
+print("")
+
+i = 0
+while i < 3:
+    i = i + 1
+    print(i)
+
+print("")
+
+def add(a, b):
+    return a + b
+print(add(3, 4))
