4d3d06f8af1a0522aa204342ec80ffbc7da65eec
PearlKang <rksk1588@gmail.com>
2021-09-19 21:55:50 +0900

    210919
    2

--- Practice/Example/02/test.py
+++ Practice/Example/02/test.py
@@ -1,5 +1,69 @@
 """
 Author: ByungWoo Kang
 Date: 2021-09-19
-이 프로그램은 Hello World를 출력하는 프로그램이다.
+이 프로그램은 숫자형 테스트 입니다.
 """
+
+#Integer
+print("Integer")
+
+a = 123
+print(a)
+
+a = -178
+print(a)
+
+a = 0
+print(a)
+
+
+#Float
+print("Float")
+
+a = 1.2
+print(a)
+
+a = -3.45
+print(a)
+
+a = 4.24E10
+print(a)
+
+a = 4.2e-10
+print(a)
+
+
+#Octal
+print("Octal")
+
+a = 0o177
+print(a)
+
+
+#Hexadecimal
+print("Hexadecimal")
+
+a = 0x8ff
+print(a)
+
+b = 0xABC
+print(a)
+
+
+#+-*/
+a = 3
+b = 4
+print("a + b = ")
+print(a + b)
+print("a * b = ")
+print(a * b)
+print("a / b = ")
+print(a / b)
+print("a ** b = ")
+print(a ** b)
+print("7 % 3 = ")
+print(7 % 3)
+print("3 % 7 = ")
+print(3 % 7)
+print("7 // 4 = ")
+print(7 // 4)
