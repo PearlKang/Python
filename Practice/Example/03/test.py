6c6a18749f7776dbb4177a3bbab469903276cefa
PearlKang <rksk1588@gmail.com>
2021-09-22 21:05:35 +0900

    210922
    update 1

--- Practice/Example/03/test.py
+++ Practice/Example/03/test.py
@@ -1,9 +1,10 @@
 """
 Author: ByungWoo Kang
-Date: 2021-09-20
+Date: 2021-09-20 ~
 이 프로그램은 문자형 테스트 입니다.
 """
 
+"""  test  """
 a = 'test'
 print(a)
 
@@ -14,4 +15,15 @@ a = '"test"'
 print(a)
 
 a = "'test'"
+print(a)
+"""  test  """
+
+
+a = "Hello World"
+print(a)
+
+a = 'Python is fun'
+print(a)
+
+a = """Life is too short, You need python"""
 print(a)
