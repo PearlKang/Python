5397d92670cdf212d99d3613c5ed5b7223e66f75
PearlKang <rksk1588@gmail.com>
2021-09-22 21:15:17 +0900

    210922
    update 2

--- Practice/Example/03/test.py
+++ Practice/Example/03/test.py
@@ -4,26 +4,71 @@ Date: 2021-09-20 ~
 이 프로그램은 문자형 테스트 입니다.
 """
 
-"""  test  """
+"""""""""  test  """""""""
 a = 'test'
 print(a)
+print('')
 
 a = "test"
 print(a)
+print('')
 
 a = '"test"'
 print(a)
+print('')
 
 a = "'test'"
 print(a)
-"""  test  """
+print('')
+"""""""""  test  """""""""
 
 
 a = "Hello World"
 print(a)
+print('')
 
 a = 'Python is fun'
 print(a)
+print('')
 
 a = """Life is too short, You need python"""
 print(a)
+print('')
+
+a = '''Life is too short, You need python'''
+print(a)
+print('')
+
+a = "Python's favorite food is perl"
+print(a)
+print('')
+
+a = '"Python is very easy." he says.'
+print(a)
+print('')
+
+a = 'Python\'s favorite food is perl'
+print(a)
+print('')
+
+a = "\"Python is very easy.\" he says."
+print(a)
+print('')
+
+a = "Life is too short\nYou need python"
+print(a)
+print('')
+
+a = '''
+Life is too short
+You need python
+'''
+print(a)
+print('')
+
+a = """
+Life is too short
+You need python
+"""
+print(a)
+print('')
