7e1f1edb9d9fca34c99fc9278aa7307b002bc014
PearlKang <rksk1588@gmail.com>
2021-09-22 21:45:16 +0900

    210922
    update 3

--- Practice/Example/03/test.py
+++ Practice/Example/03/test.py
@@ -72,3 +72,42 @@ You need python
 """
 print(a)
 print('')
+
+head = "Python"
+tail = " is fun!"
+print(head + tail + "\n")
+
+a = "Python"
+print(a*2 + "\n")
+
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+print('')
+
+a = "Life is too short"
+print(len(a))
+print('')
+
+a = "Life is too short, You need Python"
+print(a)
+print(a[0])
+print(a[-1])
+b = a[0] + a[1] + a[2] + a[3]
+print(b)
+print(a[0:4])
+print(a[0:3])
+print(a[19:])
+print(a[0:])
+print(a[:17])
+print(a[:])
+print(a[19:-7])
+print('')
+
+a = "20010331Rainy"
+date = a[:8]
+weather = a[8:]
+print(a)
+print(date)
+print(weather)
+print('')
